c33ad65aae242e841de6b0ae8b3dd767dcc07c82
Ruslan M <r1554899@gmail.com>
2020-09-05 00:28:04 +0000

    Added CONDITIONALS

--- main.py
+++ main.py
@@ -11,9 +11,26 @@ print(health)
 print(score)
 print("Hello " + name)
 
-user = input("What Is Your Name?")
-print("Hello , Nice to meet you" + user)
+user = input("What Is Your Name? ")
+print("Hello , Nice to meet you " + user)
 
-color = input("What's your favroite color?")
-print("Wow my favroite is also" + color + ", Nice!")
+color = input("What's your favroite color? ")
+print("Wow my favroite is also " + color + ", Nice!")
 
+
+
+# CONDITIONALS - if/else statements used for decision-makeing
+# tests if a conditon is true, if so do a command else so something
+
+# if color == "blue":
+#      print("The Sky Is blue")
+#  elif color == "black":
+#      print("Ah yes! The color of the night.")
+#  else:
+#       print("I geuss thats cool...")
+
+if type(color) == str:
+    print("Color is a String")
+
+if isinstance(color, str) :
+    print("Color is a String")
