b1ed4a05b23d0178f4591aa735be5ed44e3d5a44
Ruslan M <r1554899@gmail.com>
2020-09-05 00:07:31 +0000

    Added varibles

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print ("Hellow World!")
+print("hello world")
+
+# VARIBLES - a way to store a single peice of data
+# integers
+score = 0
+health = 100
+# string data type
+name = "Your Name?"
+
+print(health)
+print(score)
+print("Hello " + name)
+
+user = input("What Is Your Name?")
+print("Hello , Nice to meet you" + user)
+
+color = input("What's your favroite color?")
+print("Wow my favroite is also" + color + ", Nice!")
+
